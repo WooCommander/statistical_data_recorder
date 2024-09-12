42e006d67b36bbdce99551e506908ec4fa627560
ocnelavoc <ocnelavoc@gmail.com>
2024-09-12 03:19:12 +0300

    гзв

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -21,66 +21,79 @@ class _EditProductScreenState extends State<EditProductScreen> {
     super.initState();
     _codeController = TextEditingController(text: widget.product.code);
     _nameController = TextEditingController(text: widget.product.name);
-    _specificationController = TextEditingController(text: widget.product.specification);
-    _priceController = TextEditingController(text: widget.product.price.toString());
+    _specificationController =
+        TextEditingController(text: widget.product.specification);
+    _priceController =
+        TextEditingController(text: widget.product.price.toString());
   }
 
   @override
   Widget build(BuildContext context) {
+    // Определение, находится ли устройство в горизонтальной ориентации
+    bool isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
+    // Установка отступов: больше в горизонтальной ориентации
+    EdgeInsets padding = isLandscape
+        ? const EdgeInsets.symmetric(horizontal: 48.0)
+        : const EdgeInsets.all(16.0);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Edit Product'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          children: [
-            TextField(
-              controller: _codeController,
-              decoration: InputDecoration(labelText: 'Code'),
-            ),
-            TextField(
-              controller: _nameController,
-              decoration: InputDecoration(labelText: 'Name'),
-            ),
-            TextField(
-              controller: _specificationController,
-              decoration: InputDecoration(labelText: 'Specification'),
-            ),
-            TextField(
-              controller: _priceController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Price'),
-            ),
-            SwitchListTile(
-              title: Text('In Stock'),
-              value: widget.product.inStock,
-              onChanged: (bool value) {
-                setState(() {
-                  widget.product.inStock = value;
-                });
-              },
-            ),
-            SwitchListTile(
-              title: Text('Is Watched'),
-              value: widget.product.isWatched,
-              onChanged: (bool value) {
-                setState(() {
-                  widget.product.isWatched = value;
-                });
-              },
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // Save logic or update the state in your app
-                Navigator.pop(context);
-              },
-              child: Text('Save Changes'),
-            )
-          ],
+        appBar: AppBar(
+          title: Text('Edit Product'),
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: padding,
+            child: Column(
+              children: [
+                TextField(
+                  controller: _codeController,
+                  decoration: InputDecoration(labelText: 'Code'),
+                  readOnly: true,
+                ),
+                TextField(
+                  controller: _nameController,
+                  decoration: InputDecoration(labelText: 'Name'),
+                  readOnly: true,
+                ),
+                TextField(
+                  controller: _specificationController,
+                  decoration: InputDecoration(labelText: 'Specification'),
+                ),
+                TextField(
+                  controller: _priceController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(labelText: 'Price'),
+                ),
+                SwitchListTile(
+                  title: Text('In Stock'),
+                  value: widget.product.inStock,
+                  onChanged: (bool value) {
+                    setState(() {
+                      widget.product.inStock = value;
+                    });
+                  },
+                ),
+                SwitchListTile(
+                  title: Text('Is Watched'),
+                  value: widget.product.isWatched,
+                  onChanged: (bool value) {
+                    setState(() {
+                      widget.product.isWatched = value;
+                    });
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Save logic or update the state in your app
+                    Navigator.pop(context);
+                  },
+                  child: Text('Save Changes'),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 
   @override
